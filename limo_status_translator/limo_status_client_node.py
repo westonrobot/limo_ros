8bd80c4008d01278cf3d9a27c19fbfc7fa811fdd
Bernard Seng <bnardsyg@gmail.com>
2022-05-25 23:55:16 +0700

    fixed increment values

--- limo_status_translator/limo_status_client_node.py
+++ limo_status_translator/limo_status_client_node.py
@@ -15,25 +15,26 @@ def get_status():
     	while not rospy.is_shutdown():
 			for get_status in range(0,5):
 				if get_status == 0 : request_str = "0"
-        			rospy.loginfo(request_str)
+        			#rospy.loginfo(request_str)
         			pub.publish(request_str)
         			rate.sleep()
 				if get_status == 1 : request_str = "1"
-        			rospy.loginfo(request_str)
+        			#rospy.loginfo(request_str)
         			pub.publish(request_str)
         			rate.sleep()
 				if get_status == 2 : request_str = "2"
-        			rospy.loginfo(request_str)
+        			#rospy.loginfo(request_str)
         			pub.publish(request_str)
         			rate.sleep()
 				if get_status == 3 : request_str = "3"
-        			rospy.loginfo(request_str)
+        			#rospy.loginfo(request_str)
         			pub.publish(request_str)
         			rate.sleep()
 				if get_status == 4 : request_str = "4"
-        			rospy.loginfo(request_str)
+        			#rospy.loginfo(request_str)
         			pub.publish(request_str)
         			rate.sleep()
+				rospy.loginfo(request_str)
 				
 if __name__ == '__main__':
     try:
