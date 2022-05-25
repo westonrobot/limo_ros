d43a14f6a2c5af8f29d952b9588ecacdc1e8fd99
Bernard Seng <bnardsyg@gmail.com>
2022-05-25 22:43:41 +0700

    edited by bernard

--- limo_status_translator/limo_status_client_node.py
+++ limo_status_translator/limo_status_client_node.py
@@ -1,25 +1,40 @@
-!/usr/bin/env python
+#!/usr/bin/env python
 
 import rospy
 from std_msgs.msg import String
 
-def callback(data):
-	rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+def callback1(data):
+	rospy.loginfo(rospy.get_caller_id() + "Vehicle sate is %s", data.data)
 
-def intercept():
-    	rospy.init_node('intercept', anonymous=True)
-	sub = rospy.Subscriber('chat', String, callback)
-	pub = rospy.Publisher('chatter', String, queue_size=1)
-    	rate = rospy.Rate(10) # 10hz
+def callback2(data):
+	rospy.loginfo(rospy.get_caller_id() + "Control mode is %s", data.data)
 
-    	while not rospy.is_shutdown():
-        	intercept_str = "Intercept %s" % rospy.get_time()
-        	rospy.loginfo(intercept_str)
-        	pub.publish(intercept_str)
-        	rate.sleep()
+def callback3(data):
+	rospy.loginfo(rospy.get_caller_id() + "Battery voltage is %s", data.data)
+
+def callback4(data):
+	rospy.loginfo(rospy.get_caller_id() + "Error code is %s", data.data)
+
+def callback5(data):
+	rospy.loginfo(rospy.get_caller_id() + "Motion mode is %s", data.data)
+
+def listener():
+    rospy.init_node('client', anonymous=True)
+	rospy.Subscriber('vehicle state', String, callback1)
+	rospy.Subscriber('control mode', String, callback2)
+	rospy.Subscriber('battery voltage', String, callback3)
+	rospy.Subscriber('error code', String, callback4)
+	rospy.Subscriber('motion code', String, callback5)
+    rospy.Rate(10) # 10hz
+
+    while not rospy.is_shutdown():
+        request = "requesting" % rospy.get_time()
+        rospy.loginfo(request)
+        pub.publish(request)
+        rate.sleep()
 
 if __name__ == '__main__':
     try:
-        intercept()
+        listener()
     except rospy.ROSInterruptException:
         pass
