c3b70e52a0ff53015e8ce5d8dd0a08f5880bd422
[calvin khor] <[2102535@sit.singaporetech.edu.sg]>
2022-05-25 15:43:46 +0800

    edit

--- limo_status_translator/limo_status_translator_node.py
+++ limo_status_translator/limo_status_translator_node.py
@@ -12,23 +12,23 @@ def node():
     #Declare node name
     rospy.init_node('limo_status_translator_node.py', anonymous=True)
     #Create a Subscriber. Load the topic.
-    sub = rospy.Subscriber('chatter', String, callback)
+    sub = rospy.Subscriber('limo_status', String, callback)
     #Create Publisher('Topic name',Mold,size)
-    pub = rospy.Publisher('chatter2', String, queue_size=10)
+    #pub = rospy.Publisher('chatter2', String, queue_size=10)
     #Loop period.
     rate = rospy.Rate(10)
     
     while not rospy.is_shutdown():
         #Fill in the data to publish
-        hello_str = "hello world %s" % rospy.get_time()
+        status_str = "hello world %s" % rospy.get_time()
         #Display the data to publish in the terminal
-        rospy.loginfo(hello_str)
+        rospy.loginfo(status_str)
         #Publish data
-        pub.publish(hello_str)
+        pub.publish(status_str)
         rate.sleep()
     
 if __name__ == '__main__':
     try:
-        controller()
+        node()
     except rospy.ROSInitException:
         pass
