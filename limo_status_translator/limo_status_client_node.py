8c3783c5abc01ebd13321f6913493d10a3a4500d
Bernard Seng <bnardsyg@gmail.com>
2022-05-25 23:45:45 +0700

    added incremental request

--- limo_status_translator/limo_status_client_node.py
+++ limo_status_translator/limo_status_client_node.py
@@ -3,38 +3,40 @@
 import rospy
 from std_msgs.msg import String
 
-def callback1(data):
-	rospy.loginfo(rospy.get_caller_id() + "Vehicle sate is %s", data.data)
+def callback(data):
+	rospy.loginfo(rospy.get_caller_id() + "%s", data.data)
 
-def callback2(data):
-	rospy.loginfo(rospy.get_caller_id() + "Control mode is %s", data.data)
+def get_status():
+    	rospy.init_node('client', anonymous=True)
+	sub = rospy.Subscriber('status_from_translator', String, callback)
+	pub = rospy.Publisher('status_received', String, queue_size=1)
+    	rate = rospy.Rate(5) # 10hz
 
-def callback3(data):
-	rospy.loginfo(rospy.get_caller_id() + "Battery voltage is %s", data.data)
-
-def callback4(data):
-	rospy.loginfo(rospy.get_caller_id() + "Error code is %s", data.data)
-
-def callback5(data):
-	rospy.loginfo(rospy.get_caller_id() + "Motion mode is %s", data.data)
-
-def listener():
-    rospy.init_node('client', anonymous=True)
-	rospy.Subscriber('vehicle state', String, callback1)
-	rospy.Subscriber('control mode', String, callback2)
-	rospy.Subscriber('battery voltage', String, callback3)
-	rospy.Subscriber('error code', String, callback4)
-	rospy.Subscriber('motion code', String, callback5)
-    rospy.Rate(10) # 10hz
-
-    while not rospy.is_shutdown():
-        request = "requesting" % rospy.get_time()
-        rospy.loginfo(request)
-        pub.publish(request)
-        rate.sleep()
-
+    	while not rospy.is_shutdown():
+			for get_status in range(0,5):
+				if get_status == 0 : request_str = "0"
+        			rospy.loginfo(request_str)
+        			pub.publish(request_str)
+        			rate.sleep()
+				if get_status == 1 : request_str = "1"
+        			rospy.loginfo(request_str)
+        			pub.publish(request_str)
+        			rate.sleep()
+				if get_status == 2 : request_str = "2"
+        			rospy.loginfo(request_str)
+        			pub.publish(request_str)
+        			rate.sleep()
+				if get_status == 3 : request_str = "3"
+        			rospy.loginfo(request_str)
+        			pub.publish(request_str)
+        			rate.sleep()
+				if get_status == 4 : request_str = "4"
+        			rospy.loginfo(request_str)
+        			pub.publish(request_str)
+        			rate.sleep()
+				
 if __name__ == '__main__':
     try:
-        listener()
+        get_status()
     except rospy.ROSInterruptException:
         pass
