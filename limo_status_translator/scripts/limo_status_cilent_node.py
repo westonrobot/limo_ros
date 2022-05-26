e0e2557ef67346542d5d8823d0e6c5002b9f2447
Elson Tan Jun Hao <elsontanjunhao23@gmail.com>
2022-05-26 17:07:37 +0800

    Updated cilent and translator node

--- limo_status_translator/scripts/limo_status_cilent_node.py
+++ limo_status_translator/scripts/limo_status_cilent_node.py
@@ -1,32 +1,28 @@
-import sys
-import rospy
+#!/usr/bin/env python
+
 from std_msgs.msg import String
 from limo_status_translator.srv import GetLimoStatus
+import rospy
 
 def Limo_status_cilent():
-    rospy.init_node('limo_status_translator_node')
+    rospy.init_node('limo_status_cilent_node')
     Get_Limo_Status = rospy.ServiceProxy('Get_Limo_Status', GetLimoStatus)
     
     rate = rospy.Rate(1)
     for val in range(5):
         response = Get_Limo_Status(val)
         if response == 0:
-            pub = rospy.Publisher('/limo_ros/vehicle_state', String, queue_size=10)
+            pub0 = rospy.Publisher('/limo_ros/vehicle_state', String, queue_size=10)
         elif response == 1:
-            pub = rospy.Publisher('/limo_ros/control_mode', String, queue_size=10)
+            pub1 = rospy.Publisher('/limo_ros/control_mode', String, queue_size=10)
         elif response == 2:
-            pub = rospy.Publisher('/limo_ros/battery_voltage', String, queue_size=10)
+            pub2 = rospy.Publisher('/limo_ros/battery_voltage', String, queue_size=10)
         elif response == 3:
-            pub = rospy.Publisher('/limo_ros/error_code', String, queue_size=10)
+            pub3 = rospy.Publisher('/limo_ros/error_code', String, queue_size=10)
         elif response == 4:
-            pub = rospy.Publisher('/limo_ros/motion_mode', String, queue_size=10)  
+            pub4 = rospy.Publisher('/limo_ros/motion_mode', String, queue_size=10)  
         rate.sleep()
 
-    
-    rospy.spin()
-
 if __name__ == "__main__":
-    try:
         Limo_status_cilent()
-    except rospy.ROSInitException:
-        pass
+
