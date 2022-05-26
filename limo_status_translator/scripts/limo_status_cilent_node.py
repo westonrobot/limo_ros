a7db44ec6babe3728cd53d41a292c4229ce44c5f
Elson Tan Jun Hao <elsontanjunhao23@gmail.com>
2022-05-26 16:00:18 +0800

    Updated Cilent node

--- limo_status_translator/scripts/limo_status_cilent_node.py
+++ limo_status_translator/scripts/limo_status_cilent_node.py
@@ -1,26 +1,29 @@
 import sys
 import rospy
-#from std_msgs.msg import String
+from std_msgs.msg import String
 from limo_status_translator.srv import GetLimoStatus
 
-def Limo_status_cilent(status_string):
-    rospy.wait_for_service('Get_Limo_Status')
-    try:
-        rate = rospy.Rate(1)
-        while not rospy.is_shutdown():
-#            for x in range(5):
-#               print(x)
-            Get_Limo_Status = rospy.ServiceProxy('Get_Limo_Status', GetLimoStatus)
-            resp1 = Get_Limo_Status(status_string)
-            pub = rospy.Publisher('vehicle_state', resp1, queue_size=10)
-            pub1 = rospy.Publisher('control_mode', resp1, queue_size=10)
-            pub2 = rospy.Publisher('battery_voltage', resp1, queue_size=10)
-            pub3 = rospy.Publisher('error_code', resp1, queue_size=10)
-            pub4 = rospy.Publisher('motion_mode', resp1, queue_size=10)
-            rate.sleep()
-            
-    except rospy.ServiceException as e:
-        print("Service call failed: %s"%e)
+def Limo_status_cilent():
+    rospy.init_node('limo_status_translator_node')
+    Get_Limo_Status = rospy.ServiceProxy('Get_Limo_Status', GetLimoStatus)
+    
+    rate = rospy.Rate(1)
+    for val in range(5):
+        response = Get_Limo_Status(val)
+        if response == 0:
+            pub = rospy.Publisher('/limo_ros/vehicle_state', String, queue_size=10)
+        elif response == 1:
+            pub = rospy.Publisher('/limo_ros/control_mode', String, queue_size=10)
+        elif response == 2:
+            pub = rospy.Publisher('/limo_ros/battery_voltage', String, queue_size=10)
+        elif response == 3:
+            pub = rospy.Publisher('/limo_ros/error_code', String, queue_size=10)
+        elif response == 4:
+            pub = rospy.Publisher('/limo_ros/motion_mode', String, queue_size=10)  
+        rate.sleep()
+
+    
+    rospy.spin()
 
 if __name__ == "__main__":
     try:
