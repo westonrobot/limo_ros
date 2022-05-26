660ea987b3ddd9e5f73d3d82752dcf08c8fda46b
tf <stmok00@protonmail.com>
2022-05-26 10:32:41 +0800

    coordinate timing btw svc and client - unfinished

--- limo_status_translator/limo_status_translator_node.py
+++ limo_status_translator/limo_status_translator_node.py
@@ -3,21 +3,27 @@
 import rospy
 from std_msgs.msg import String
 
-def callback1(data):
+# callback from client
+def callback_client(data):
+	rospy.loginfo("%s", data.data)
+
+# callback to limo
+def callback_limo(data):
 	rospy.loginfo("%s", data.data)
 
 def status_string():
 	rospy.init_node('limo_status_translator_node', anonymous=True)
-	#sub1 = rospy.Subscriber('limo_status', String, callback2)
-	sub2 = rospy.Subscriber('status_received', String, callback1)
-	pub = rospy.Publisher('status_from_translator', String, queue_size=1)
-	rate = rospy.Rate(1) # 10hz
+	#sub_limo = rospy.Subscriber('limo_status', String, callback_limo)
+	sub_client = rospy.Subscriber('status_received', String, callback_client)
+	pub_client = rospy.Publisher('status_from_translator', String, queue_size=0)
+	rate = rospy.Rate(1)
 
 	while not rospy.is_shutdown():
+		# communicate to client - return a string
 		rtn_str = "STATUS"
-		pub.publish(rtn_str)
+		pub_client.publish(rtn_str)
 		rate.sleep()
-		rospy.loginfo(rtn_str)
+		rospy.loginfo(rtn_str + " sent")
 
 if __name__ == '__main__':
     try:
