bb57ec57820b5253e63322f2fd2ee3466e746dac
“[calvinkhor]” <2102535@sit.singaporetech.edu.sg>
2022-05-26 11:01:12 +0800

    Should be the correct code just need check the import

--- limo_status_translator/limo_status_translator_node.py
+++ limo_status_translator/limo_status_translator_node.py
@@ -1,34 +1,50 @@
 #!/usr/bin/env python
 # coding: utf-8
-import rospy
-from std_msgs.msg import String
 
-def callback(data):
-    #Display the received data in the terminal
-    #Data is data.Received in data
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-      
-def node():
-    #Declare node name
-    rospy.init_node('limo_status_translator_node.py', anonymous=True)
-    #Create a Subscriber. Load the topic.
-    sub = rospy.Subscriber('limo_status', String, callback)
-    #Create Publisher('Topic name',Mold,size)
-    #pub = rospy.Publisher('chatter2', String, queue_size=10)
-    #Loop period.
-    rate = rospy.Rate(10)
-    
-    while not rospy.is_shutdown():
-        #Fill in the data to publish
-        status_str = "hello world %s" % rospy.get_time()
-        #Display the data to publish in the terminal
-        rospy.loginfo(status_str)
-        #Publish data
-        pub.publish(status_str)
-        rate.sleep()
-    
-if __name__ == '__main__':
-    try:
-        node()
-    except rospy.ROSInitException:
+
+    #Subscribe to the “/limo_status” topic.
+    #Process the message received from the “/limo_status” topic into a “human readable string”
+    #Implement a service (.srv file given), that will respond with the correct string depending on what was requested.
+    #e.g. if get_status = 4, that means the client is requesting for the status_string corresponding to the robot’s current motion_mode
+
+
+
+
+
+import rospy
+
+from GetLimoStatus.srv import (GET_STATUS_VEHICLE_STATE, GET_STATUS_CONTROL_MODE, 
+                                         GET_STATUS_BATTERY_VOLTAGE, GET_STATUS_ERROR_CODE, GET_STATUS_MOTION_MODE)
+
+
+def __init__(self):
         pass
+def get_status_vehicle_state(self,req):
+        rospy.loginfo('Vehicle State    ')
+        response = 'GetLimoStatus.srv'.GET_STATUS_VEHICLE_STATERESPONSE()
+        return response
+def get_status_control_mode(self,req):
+        rospy.loginfo('Control Mode    ')
+        response = 'GetLimoStatus.srv'.GET_STATUS_CONTROL_MODERESPONSE()
+        return response    
+def get_status_battery_voltage(self,req):
+        rospy.loginfo('Battery Voltage    ')
+        response = 'GetLimoStatus.srv'.GET_STATUS_BATTERY_VOLTAGERESPONSE()
+        return response
+def get_status_error_code(self,req):
+        rospy.loginfo('error code    ')
+        response = 'GetLimoStatus.srv'.GET_STATUS_ERROR_CODERESPONSE()
+        return response   
+def get_status_motion_mode(self,req):
+        rospy.loginfo('Motion Mode    ')
+        response = 'GetLimoStatus.srv'.GET_STATUS_MOTION_MODERESPONSE()
+        return response    
+
+def node():
+        rospy.init_node('limo_status_translator_node', anoymous=True)
+        get_status_vehicle_state = rospy.Service('get_status_vehicle_state', GET_STATUS_VEHICLE_STATE)   
+        if __name__ == '__main__':
+            try:
+                node()
+            except rospy.ROSInitException:
+                pass
