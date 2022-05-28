3d228f11726a134406bb6fdfefd822e9440a6122
Ellice <2102779@sit.singaporetech.edu.sg>
2022-05-28 22:37:23 +0800

    Corrected get_status in translator.py

--- limo_status_translator/scripts/translator.py
+++ limo_status_translator/scripts/translator.py
@@ -9,25 +9,29 @@ def callback(req):
 
     if req.get_status == 0:
         # hello_str = "Vehicle status is " + str(req.get_status)
-        if req.vehicle_state == 0x00:
+        print("vehicle state")
+        if req.GET_STATUS_VEHICLE_STATE == 0x00:
             status_str = "System normal"
         else:
             status_str = hello_str + "System exception"
     elif req.get_status == 1:
-        if req.vehicle_state == 0x00:
+        print("control mode")
+        if req.GET_STATUS_CONTROL_MODE == 0x00:
             status_str = "Standby"
-        elif req.vehicle_state == 0x01:
+        elif req.GET_STATUS_CONTROL_MODE == 0x01:
             status_str = "Command Control"
-        elif req.vehicle_state == 0x02:
+        elif req.GET_STATUS_CONTROL_MODE == 0x02:
             status_str = "App Control"
-        elif req.vehicle_state == 0x03:
+        elif req.GET_STATUS_CONTROL_MODE == 0x03:
             status_str = "Remote Control"
     elif req.get_status == 2:
-            status_str = "Battery voltage level: " + str(req.battery_voltage)
+            status_str = "Battery voltage level: " + str(req.GET_STATUS_BATTERY_VOLTAGE)
+            print("battery volt")
     elif req.get_status == 3:
             #bit 0 and bit 1 will not be accessed cuz it's 0b
             #access the bits from [2] onwards to [11]
-            error_code = bin(req.error_code)
+            error_code = bin(req.GET_STATUS_ERROR_CODE)
+            print(type(error_code))
             if(error_code[2] == 1):
                 status_str = "Battery undervoltage fault"
             elif(error_code[3] == 1):
@@ -47,12 +51,13 @@ def callback(req):
             elif(error_code[10] == 1):
                 status_str = "Upper layer communication status"
     elif req.get_status == 4:
-        if req.motion_mode == 0x00:
+        print("motion mode")
+        if req.GET_STATUS_MOTION_MODE == 0x00:
             status_str = "Motion mode is 4-wheel differential"
-        elif req.motion_mode == 0x01:
+        elif req.GET_STATUS_MOTION_MODE == 0x01:
             status_str = "Motion mode is Ackerman"
-        elif req.motion_mode == 0x02:
-            status_str = "Motion mode is Mecanum"    
+        elif req.GET_STATUS_MOTION_MODE == 0x02:
+            status_str = "Motion mode is Mecanum" 
 
     rospy.loginfo(rospy.get_caller_id() + 'I heard %s', req.get_status)
     resp = GetLimoStatusResponse()
